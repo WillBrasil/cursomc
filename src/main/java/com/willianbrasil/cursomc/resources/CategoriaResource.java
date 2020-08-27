3c2244ebc28b62d4863a0e8b4771290609f8beea
Willian Brasil <brasil-will@hotmail.com>
2020-08-27 16:13:02 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/willianbrasil/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/willianbrasil/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -35,7 +37,8 @@ public class CategoriaResource {
 	}
 	// METODO POST
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+	public ResponseEntity<Void> insert(@RequestBody @Valid CategoriaDTO objDto){
+		Categoria obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -44,7 +47,8 @@ public class CategoriaResource {
 	}
 	//METODO PUT
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+	public ResponseEntity<Void> update(@RequestBody @Valid CategoriaDTO objDto, @PathVariable Integer id){
+		Categoria obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
@@ -65,7 +69,7 @@ public class CategoriaResource {
 		
 	}
 	
-	//METODO PARA PAGINACAO
+	//METODO PARA PAGINACAO OPCIONAL DE REQUISICAO
 	@RequestMapping(value ="/page", method=RequestMethod.GET)
 	public ResponseEntity<Page<CategoriaDTO>> findPage(
 		  @RequestParam(value="page", defaultValue="0")	Integer page,
--- src/main/java/com/willianbrasil/cursomc/services/CategoriaService.java
+++ src/main/java/com/willianbrasil/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.willianbrasil.cursomc.domain.Categoria;
+import com.willianbrasil.cursomc.dto.CategoriaDTO;
 import com.willianbrasil.cursomc.repositories.CategoriaRepository;
 import com.willianbrasil.cursomc.services.exceptions.DataIntegrityException;
 import com.willianbrasil.cursomc.services.exceptions.ObjectNotFoundException;
@@ -59,6 +60,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
