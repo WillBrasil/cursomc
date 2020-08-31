ca1dfeebe575abfa105192e067ce2957023bf8c2
Willian Brasil <brasil-will@hotmail.com>
2020-08-31 12:56:39 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

---
+++ src/main/java/com/willianbrasil/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.willianbrasil.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.willianbrasil.cursomc.domain.Cliente;
+import com.willianbrasil.cursomc.dto.ClienteDTO;
+import com.willianbrasil.cursomc.repositories.ClienteRepository;
+import com.willianbrasil.cursomc.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO>{
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+	
+	@Autowired
+	public ClienteRepository repo;
+	
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		
+		//VERIFICACAO DE EMAIL
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email ja existente"));
+		}
+		
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
